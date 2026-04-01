7e1295fea863028d26b7988f7583cca4e0790763
Jakub <kubakarwow@gmail.com>
2026-04-01 12:16:28 +0200

    removed security for tests v2

--- src/main/java/com/example/medicalproject/Utils/SecurityConfig.java
+++ src/main/java/com/example/medicalproject/Utils/SecurityConfig.java
@@ -1,34 +1,34 @@
-package com.example.medicalproject.Utils;
-
-import com.example.medicalproject.services.JwtService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-public class SecurityConfig {
-
-    private final JwtService jwtService;
-
-    public SecurityConfig(JwtService jwtService) {
-        this.jwtService = jwtService;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login").permitAll()
-                        .anyRequest().authenticated()
-                )
+//package com.example.medicalproject.Utils;
+//
+//import com.example.medicalproject.services.JwtService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    private final JwtService jwtService;
+//
+//    public SecurityConfig(JwtService jwtService) {
+//        this.jwtService = jwtService;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        return http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/auth/login").permitAll()
+//                        .anyRequest().authenticated()
+//                )
 //                .addFilterBefore(
 //                        new JwtFilter(jwtService),
 //                        UsernamePasswordAuthenticationFilter.class
 //                )
-                .build();
-    }
-}
+//                .build();
+//    }
+//}
