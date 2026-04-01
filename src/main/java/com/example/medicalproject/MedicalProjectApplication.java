9ffb97c6dff930d2317005838a9c49b6c8d41008
Jakub <kubakarwow@gmail.com>
2026-04-01 16:01:35 +0200

    security done v2

--- src/main/java/com/example/medicalproject/MedicalProjectApplication.java
+++ src/main/java/com/example/medicalproject/MedicalProjectApplication.java
@@ -3,7 +3,10 @@ package com.example.medicalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+})
 public class MedicalProjectApplication {
 
     public static void main(String[] args) {
